607191fe9b358f2b65757695e73e4f40ab7a64ef
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-03-02 17:36:55 -0600

    ok

--- StreamingGuide.py
+++ StreamingGuide.py
@@ -3,59 +3,83 @@
 #Github user: ParraL1
 #Description: Movie streaming service
 
-def __init__(self,title,genre,director,year):
-    self._title=title
-    self._genre=genre
-    self._director=director
-    self._year=year
+# Movie Class
+class Movie:
+    def __init__(self, title, genre, director, year):
+
+        self._title = title
+        self._genre = genre
+        self._director = director
+        self._year = year
+
+    # movie's title
     def get_title(self):
         return self._title
+
+    # movie's genre
     def get_genre(self):
         return self._genre
+
+    # movie's director
     def get_director(self):
         return self._director
+
+    # movie's year
     def get_year(self):
         return self._year
 
+
+    def __str__(self):
+        return self._title+"("+str(self._year)+"), Director: "+self._director+", Genre: "+self._genre
+
+# StreamingService
 class StreamingService:
-    def __init__(self,name):
-        self.name=name
-        self.catalog=dict()
+    def __init__(self, name):
+        self._name = name
+        self._catalog = []
+
     def get_name(self):
-        return self.name
+        return self._name
+
     def get_catalog(self):
-        return self.catalog
-    def add_movie(self,movie):
-        movietitle=movie.get_title()
-        self.catalog[movietitle]=movie
-    def delete_movie(self,title):
-        if title in self.catalog.keys():
-            del self.catalog[title]
+        return self._catalog
+
+    def add_movie(self, movie):
+        self._catalog.append(movie)
+
+    def delete_movie(self, movieTitle):
+        for movie in self._catalog:
+            if movie.get_title() == movieTitle:
+                self._catalog.remove(movie)
+
 
 class StreamingGuide:
     def __init__(self):
-        self._data=[]
-    def add_streaming_service(self,streamingservice):
-        self._data.append(streamingservice)
-    def delete_streaming_service(self,name):
-        for i in self._data:
-                if(i.get_name()==name):
-                    self._data.remove(i)
-                    break
-    def where_to_watch(self,title):
-        l=[]
-    for i in self._data:
-        cata=i.get_catalog()
-        if title in cata.keys():
-            mobj=cata[title]
-        ye=mobj.get_year()
-        s1=str(title)+ "(" + str(ye) + ")"
-    l.append(s1)
-    if len(self._data)==0:
-        l.append("None")
-    else:
-            for a in self._data:
-                cat=a.get_catalog()
-    if title in cat.keys():
-          l.append(a.get_name())
-        return l
+        self._services = []
+
+    def add_streaming_service(self, service):
+        self._services.append(service)
+
+    def delete_streaming_service(self, serviceName):
+        for service in self._services:
+            if service.get_name() == serviceName:
+                self._services.remove(service)
+
+    def where_to_watch(self, title):
+        result = []
+        found = False
+
+        for i in range(len(self._services)):
+            catalog = self._services[i].get_catalog()
+            for movie in catalog:
+                if movie.get_title() == title:
+                    if found == False:
+                        desired_movie = title + "("+str(movie.get_year())+")"
+                        result.append(desired_movie)
+                        found = True
+                    result.append(self._services[i].get_name())
+
+        if found == True:
+            return result
+        else:
+            return None
